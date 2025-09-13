23d4a31ba37b9cf5ca4c680fd4a908187d715286
ngocprofile <146452461+ngocprofile@users.noreply.github.com>
2025-09-13 14:57:30 +0700

    code sau khi update

--- demo.js
+++ demo.js
@@ -1,4 +1,20 @@
-Hoten = prompt("Nhập họ tên: ");
-tuoi = prompt("Nhập tuổi: ");
-console.log("Họ tên: " + Hoten);
-console.log("Tuổi: " + tuoi);
+const { get } = require("http");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function tinhTuoi(namsinh){
+    const namhientai = new Date().getFullYear();
+    return namhientai - namsinh;
+}
+
+rl.question("Nhập họ tên: ", (Hoten) => {
+    rl.question("Nhập năm sinh: ", (namsinh) => {
+        console.log("Họ tên: " + Hoten);
+        console.log("Tuổi: " + tinhTuoi(namsinh));
+        rl.close();
+    });
+});
